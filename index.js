a84be06a286cd1a89e658098ea5ee08e3bfca43a
Fahmid <fahmidhossainsakib@gmail.com>
2023-03-06 18:35:56 +0900

    changes done in db connecting code

--- index.js
+++ index.js
@@ -13,56 +13,17 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@handwriting-dataset.wkyksjq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
-async function run() {
-    try {
-        await client.connect();
-        console.log("connected");
-        const userInputCollection = client.db("handWrDataset").collection("userInput");
-
-        app.post('/user', async (req, res) => {
-            const newInput = req.body;
-            console.log(newInput)
-            const result = await userInputCollection.insertOne(newInput);
-            res.send({ result });
-        })
-
-        app.post('/test', async (req, res) => {
-            const newInput = req.body;
-            console.log(newInput)
-            const result = await userInputCollection.insertOne(newInput);
-            res.send({ result });
-        })
-    }
-    finally {
-        // await client.close();
-    }
-
-}
-run().catch(console.dir);
-
-
-
 app.get('/hello', (req, res) => {
     res.send('Hello handwriting dataset port a: ' + port);
 })
 
-// app.get('/users', (req, res) => {
-//     console.log('front end client requesting for data ')
-//     // res.send(users);
-// });
-
-
-// app.get('/user/:id', (req, res) => {
-//     // const id = parseInt(req.params.id);
-//     // console.log('print id, ' + id + ", " + typeof (id));
-//     // user = users.find(u => u.id === id);
-//     console.log("default api triggered !");
-//     res.send({response : "connected"});
-// })
-
-
-
+app.post('/user', async (req, res) => {
+    await client.connect();
+    const userInputCollection = client.db("handWrDataset").collection("userInput");
+    const newInput = req.body;
+    const result = await userInputCollection.insertOne(newInput);
+    res.send({ result });
+})
 
 app.listen(port, () => {
     console.log('Example app listening to port: ', port)
